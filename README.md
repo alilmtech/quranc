e2aa8b7bc0efc68e3a3fdcb4f35c81f82ee94152
jsteenb2 <jonathansteenbergen@gmail.com>
2019-07-21 21:22:31 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# quranc - quran.com API client
+
+As of this writing, the full api is supported. Even some of the api endpoints that are documented incorrectly are supported. All comments, issues, and PRs are welcome.
--- client.go
+++ client.go
@@ -1,7 +1,9 @@
 package quranc
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"errors"
 	"net/http"
 	"sort"
@@ -11,6 +13,22 @@ import (
 	"github.com/jsteenb2/httpc"
 )
 
+type QuranAPI interface {
+	Recitations(ctx context.Context, reqOpts ...ReqOptFn) ([]Recitation, error)
+	Translations(ctx context.Context, reqOpts ...ReqOptFn) ([]Translation, error)
+	Languages(ctx context.Context, reqOpts ...ReqOptFn) ([]Language, error)
+	Tafsiraat(ctx context.Context, reqOpts ...ReqOptFn) ([]Tafsir, error)
+	Chapters(ctx context.Context, reqOpts ...ReqOptFn) ([]Chapter, error)
+	Chapter(ctx context.Context, id int, reqOpts ...ReqOptFn) (Chapter, error)
+	ChapterInfo(ctx context.Context, id int, reqOpts ...ReqOptFn) (ChapterInfo, error)
+	Verses(ctx context.Context, chapterID int, reqOpts ...VersesReqOptFn) ([]Verse, error)
+	Verse(ctx context.Context, chapterID, verseID int) (Verse, error)
+	Juzzah(ctx context.Context) ([]Juz, error)
+	VerseTafsir(ctx context.Context, chapterID, verseID int, reqOpts ...VerseTafsirReqOptFn) ([]VerseTafsir, error)
+	Search(ctx context.Context, query SearchRequest) (SearchResponse, error)
+}
+
+// Doer is an interface to abstract the http client out to its basic functionality.
 type Doer interface {
 	Do(*http.Request) (*http.Response, error)
 }
@@ -19,8 +37,10 @@ type clientOpt struct {
 	host string
 }
 
+// ClientOptFn is an option to set the options of the client constructor.
 type ClientOptFn func(opt clientOpt) clientOpt
 
+// ClientHost sets the host for the client url.
 func ClientHost(host string) ClientOptFn {
 	return func(opt clientOpt) clientOpt {
 		opt.host = host
@@ -28,13 +48,17 @@ func ClientHost(host string) ClientOptFn {
 	}
 }
 
+// Client is the API client  that translates the quran.com api into familiar go types.
 type Client struct {
 	c *httpc.Client
 }
 
+// New Constructs a new Client. All default options will be  used if no options are
+// provided to overwrite them. The defaults are:
+//	host: https://quran.com/api
 func New(doer Doer, opts ...ClientOptFn) *Client {
 	opt := clientOpt{
-		host: "http://staging.quran.com:3000",
+		host: "https://quran.com/api",
 	}
 	for _, o := range opts {
 		opt = o(opt)
@@ -46,6 +70,7 @@ func New(doer Doer, opts ...ClientOptFn) *Client {
 	}
 }
 
+// Recitation is a recitation provided from quran.com.
 type Recitation struct {
 	ID                    int    `json:"id"`
 	Style                 string `json:"style"`
@@ -53,11 +78,7 @@ type Recitation struct {
 	ReciterNameTranslated string `json:"reciter_name_translated"`
 }
 
-type TranslatedName struct {
-	LanguageName string `json:"language_name"`
-	Name         string `json:"name"`
-}
-
+// Recitations returns all the available quran.com recitations.
 func (c *Client) Recitations(ctx context.Context, reqOpts ...ReqOptFn) ([]Recitation, error) {
 	var opt reqOpt
 	for _, optFn := range reqOpts {
@@ -78,6 +99,8 @@ func (c *Client) Recitations(ctx context.Context, reqOpts ...ReqOptFn) ([]Recita
 	return resp.Recitations, nil
 }
 
+// Translation is a translation available via the quran.com api. The translation's ID  maybe used in
+// other api calls to add translations to the response.
 type Translation struct {
 	ID           int    `json:"id"`
 	AuthorName   string `json:"author_name"`
@@ -86,6 +109,7 @@ type Translation struct {
 	Slug         string `json:"slug"`
 }
 
+// Translations returns all the available quran.com translations.
 func (c *Client) Translations(ctx context.Context, reqOpts ...ReqOptFn) ([]Translation, error) {
 	var opt reqOpt
 	for _, optFn := range reqOpts {
@@ -111,6 +135,15 @@ func (c *Client) Translations(ctx context.Context, reqOpts ...ReqOptFn) ([]Trans
 	return resp.Translations, nil
 }
 
+// TranslatedName is a name and the language is is translated from.
+type TranslatedName struct {
+	LanguageName string `json:"language_name"`
+	Name         string `json:"name"`
+}
+
+// Language is the a language with associated quran.com identifiers. The language ID is
+// useful in filtering other api calls to the language provided. The iso code is useful
+// in other contexts like search and verses calls.
 type Language struct {
 	ID              int              `json:"id"`
 	Name            string           `json:"name"`
@@ -120,6 +153,7 @@ type Language struct {
 	TranslatedNames []TranslatedName `json:"translated_names"`
 }
 
+// Languages returns all the available quran.com languages.
 func (c *Client) Languages(ctx context.Context, reqOpts ...ReqOptFn) ([]Language, error) {
 	var opt reqOpt
 	for _, optFn := range reqOpts {
@@ -145,6 +179,8 @@ func (c *Client) Languages(ctx context.Context, reqOpts ...ReqOptFn) ([]Language
 	return resp.Languages, nil
 }
 
+// Tafsir is a tafsir overview available from quran.com. Food for thought, the slug
+// is never populated but is "supported" through the docs, but not in reality.
 type Tafsir struct {
 	ID           int    `json:"id"`
 	AuthorName   string `json:"author_name"`
@@ -153,6 +189,7 @@ type Tafsir struct {
 	LanguageName string `json:"language_name"`
 }
 
+// Tafsiraat returns all the available quran.com tafsiraat.
 func (c *Client) Tafsiraat(ctx context.Context, reqOpts ...ReqOptFn) ([]Tafsir, error) {
 	var opt reqOpt
 	for _, optFn := range reqOpts {
@@ -178,6 +215,8 @@ func (c *Client) Tafsiraat(ctx context.Context, reqOpts ...ReqOptFn) ([]Tafsir, 
 	return resp.Tafsirs, nil
 }
 
+// Chapter or surah along with its relevant metadata combine to detail the summary of the
+// chatper as a whole.
 type Chapter struct {
 	ID              int    `json:"id"`
 	ChapterNumber   int    `json:"chapter_number"`
@@ -231,6 +270,7 @@ func apiChapterToChapter(ch apiChapter) Chapter {
 	}
 }
 
+// Chapters returns the available chapters from quran.com.
 func (c *Client) Chapters(ctx context.Context, reqOpts ...ReqOptFn) ([]Chapter, error) {
 	var opt reqOpt
 	for _, optFn := range reqOpts {
@@ -261,6 +301,7 @@ func (c *Client) Chapters(ctx context.Context, reqOpts ...ReqOptFn) ([]Chapter, 
 	return chapters, nil
 }
 
+// Chapters returns the the given chapter by id from quran.com.
 func (c *Client) Chapter(ctx context.Context, id int, reqOpts ...ReqOptFn) (Chapter, error) {
 	var opt reqOpt
 	for _, optFn := range reqOpts {
@@ -396,103 +437,120 @@ type (
 	VersesReqOptFn func(opts versesReqOpt) versesReqOpt
 
 	versesReqOpt struct {
-		language   string
-		recitation int
-		textType   string
+		Language   string
+		Recitation int
+		TextType   string
 
-		page   int
-		limit  int
-		offset int
+		Page   int
+		Limit  int
+		Offset int
 
-		media        []int
-		translations []int
+		Media        []int
+		Translations []int
 	}
 )
 
 func (v versesReqOpt) queryParams(r *httpc.Request) *httpc.Request {
-	if v.language != "" {
-		r = r.QueryParam("language", v.language)
+	if v.Language != "" {
+		r = r.QueryParam("language", v.Language)
 	}
 
-	if v.recitation > 0 {
-		r = r.QueryParam("recitation", strconv.Itoa(v.recitation))
+	if v.Recitation > 0 {
+		r = r.QueryParam("recitation", strconv.Itoa(v.Recitation))
 	}
 
-	if v.textType != "" {
-		r = r.QueryParam("text_type", v.textType)
+	if v.TextType != "" {
+		r = r.QueryParam("text_type", v.TextType)
 	}
 
-	if v.page > 0 {
-		r = r.QueryParam("page", strconv.Itoa(v.page))
+	if v.Page > 0 {
+		r = r.QueryParam("page", strconv.Itoa(v.Page))
 	}
 
-	if v.limit > 0 {
-		r = r.QueryParam("limit", strconv.Itoa(v.limit))
+	if v.Limit > 0 {
+		r = r.QueryParam("limit", strconv.Itoa(v.Limit))
 	}
 
-	for _, media := range v.media {
+	for _, media := range v.Media {
 		r = r.QueryParam("media[]", strconv.Itoa(media))
 	}
 
-	for _, translation := range v.translations {
+	for _, translation := range v.Translations {
 		r = r.QueryParam("translations[]", strconv.Itoa(translation))
 	}
 
 	return r
 }
 
+func (v versesReqOpt) key(chapterID int) ([]byte, error) {
+	sort.Ints(v.Media)
+	sort.Ints(v.Translations)
+
+	input := struct {
+		VerseReqOpts versesReqOpt
+		ChapterID    int
+	}{
+		VerseReqOpts: v,
+		ChapterID:    chapterID,
+	}
+
+	var buf bytes.Buffer
+	err := gob.NewEncoder(&buf).Encode(input)
+	return buf.Bytes(), err
+}
+
 func VersesLanguage(isoCode string) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.language = isoCode
+		opts.Language = isoCode
 		return opts
 	}
 }
 
 func VersesRecitation(recitation int) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.recitation = recitation
+		opts.Recitation = recitation
 		return opts
 	}
 }
 
 func VersesTextType(textType string) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.textType = textType
+		opts.TextType = textType
 		return opts
 	}
 }
 
 func VersesLimit(i int) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.limit = i
+		opts.Limit = i
 		return opts
 	}
 }
 
 func VersesOffset(i int) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.offset = i
+		opts.Offset = i
 		return opts
 	}
 }
 
 func VersesPage(i int) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.page = i
+		opts.Page = i
 		return opts
 	}
 }
 
 func VersesMedia(media []int) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.media = media
+		opts.Media = media
 		return opts
 	}
 }
 
 func VersesTranslations(translations []int) VersesReqOptFn {
 	return func(opts versesReqOpt) versesReqOpt {
-		opts.translations = translations
+		opts.Translations = translations
 		return opts
 	}
 }
